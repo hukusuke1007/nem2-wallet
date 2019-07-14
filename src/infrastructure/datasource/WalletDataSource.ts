47e55d081c39aac0bf60b7cc2611e498aa89ad93
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-07-14 18:22:13 +0900

    Update WalletDataSource.ts

--- src/infrastructure/datasource/WalletDataSource.ts
+++ src/infrastructure/datasource/WalletDataSource.ts
@@ -23,19 +23,18 @@ export class WalletDataSource implements WalletRepository {
     const item: any = await localForage.getItem(this.localStorageKey)
     console.log('loadWallet', item)
     if (item !== null) {
-      const result = new Wallet()
-      result.address = 'address' in item ? item.address : undefined
-      result.privateKey = 'privateKey' in item ? item.privateKey : undefined
-      result.publicKey = 'publicKey' in item ? item.publicKey : undefined
-      result.networkType = 'networkType' in item ? item.networkType : undefined
-      return result
+      return new Wallet(
+        'address' in item ? item.address : undefined,
+        'publicKey' in item ? item.publicKey : undefined,
+        'privateKey' in item ? item.privateKey : undefined,
+        'networkType' in item ? item.networkType : undefined,
+      )
     } else {
       return undefined
     }
   }
 
   async loadBalance(addr: string) {
-    console.log('loadBalance')
     try {
       return await this.wrapper.loadBalance(addr)
     } catch (error) {
