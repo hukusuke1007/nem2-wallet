bdfed7b954acba5ed58db80b888107492ebbd3bf
Shohei Nakagawa <hukusuke1007@gmail.com>
2019-07-15 10:25:18 +0900

    update

---
+++ docs/precache-manifest.a05f7ea25af40857d6262a44917fb356.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9b1c4c6c92d4f26b9e3",
+    "url": "/nem2-wallet/css/app.a39d42a6.css"
+  },
+  {
+    "revision": "d9b1c4c6c92d4f26b9e3",
+    "url": "/nem2-wallet/js/app.034f05f6.js"
+  },
+  {
+    "revision": "1a0447bd47d440a36ce1",
+    "url": "/nem2-wallet/css/chunk-vendors.f190deb1.css"
+  },
+  {
+    "revision": "1a0447bd47d440a36ce1",
+    "url": "/nem2-wallet/js/chunk-vendors.dc6bce24.js"
+  },
+  {
+    "revision": "d8d327bc5c3d98e6e4374eb6db3f262a",
+    "url": "/nem2-wallet/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nem2-wallet/robots.txt"
+  }
+];
